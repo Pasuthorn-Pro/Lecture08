0f2abcab913a7e1d4db28f691eb065144c943b2f
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-31 23:50:02 +0700

    Lecture08

--- Code/(pg14)File read.py
+++ Code/(pg14)File read.py
@@ -1,10 +1,14 @@
 def main():
+    #Open file philosopher.txt
     infile = open("philosophers", "r")
 
+    #Read the file's contents.
     file_contents = infile.read()
 
+    #Close the file
     infile.close()
 
+    #Print the data that was read into memory.
     print(file_contents)
 
 main()
