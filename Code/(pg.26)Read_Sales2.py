74b116c5eb9e40380b3138a4851a6fb71a15ee59
Pasuthorn-Pro <pasuthorn1406@gmail.com>
2024-09-05 09:43:59 +0700

    Lecture08

--- Code/(pg.26)Read_Sales2.py
+++ Code/(pg.26)Read_Sales2.py
@@ -1,4 +1,11 @@
 with open("sales.txt", "r") as sales_file:
-    for line in sales_file:
-        amount = float(line)
-        print(format(amount, ".2f"))
+    sales_file = sales_file.readlines()
+
+total = 0
+
+for line in sales_file:
+    amount = float(line)
+    print(format(amount, ".2f"))
+    total += float(line.strip())
+
+print(f"The total sales of al day is: ${total: .2f}")
